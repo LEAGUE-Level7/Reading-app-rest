fbfe102f1ff9b7c29e4d7f2ec15100b62ae5d919
Ankit Bisain <ankit.bisain@gmail.com>
2020-04-06 20:02:08 -0700

    made downloadtest

---
+++ src/main/java/org/llamas/readingapp/DownloadTest.java
@@ -0,0 +1,21 @@
+package org.llamas.readingapp;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DownloadTest {
+	
+	@GetMapping("/apple")
+	String getPage() {
+		return "hello";
+	}
+	
+}
